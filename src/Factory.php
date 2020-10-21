47d8542997b3a5611d7f27a8372a02cedd73f5b2
Wes <wes@fusionspim.com>
2020-10-21 07:36:50 +0100

    white space

--- src/Factory.php
+++ src/Factory.php
@@ -12,55 +12,53 @@ class Factory
     public const DEFAULT_EXCLUDED_NAME = 'AcceptanceTesterActions.php'; // Annotated with @codingStandardsIgnoreFile
 
     public const DEFAULT_RULES = [
-        '@PSR2'                                      => true,
-        '@PhpCsFixer'                                => true,
-        'array_syntax'                               => ['syntax' => 'short'],
-        'backtick_to_shell_exec'                     => true,
-        'binary_operator_spaces'                     => ['default' => 'align_single_space'],
-        'blank_line_after_opening_tag'               => false, // We prefer the opposite to @PhpCsFixer
-        'blank_line_before_statement'                => [
-            'statements' => ['case', 'for', 'foreach', 'if', 'return', 'switch', 'try', 'while'],
-        ],
-        'class_attributes_separation'                => ['elements' => ['method']], // We like to group const/property
-        'class_keyword_remove'                       => false, // We like IDEs picking up usage via ::class
-        'combine_nested_dirname'                     => true,
-        'concat_space'                               => ['spacing' => 'one'],
-        'date_time_immutable'                        => true,
-        'declare_strict_types'                       => false,
-        'dir_constant'                               => true,
-        'ereg_to_preg'                               => true,
-        'error_suppression'                          => true,
-        'escape_implicit_backslashes'                => false, // Too many changes for now
-        'function_to_constant'                       => true,
-        'general_phpdoc_annotation_remove'           => ['annotations' => []],
-        'heredoc_indentation'                        => false, // Too many changes for now
-        'heredoc_to_nowdoc'                          => false, // Too many changes for now
-        'implode_call'                               => true,
-        'increment_style'                            => false, // Sometimes either is appropriate
-        'linebreak_after_opening_tag'                => true,
-        'logical_operators'                          => true,
-        'method_argument_space'                      => ['on_multiline' => 'ignore'],
-        'mb_str_functions'                           => true,
-        'modernize_types_casting'                    => true,
-        'multiline_whitespace_before_semicolons'     => ['strategy' => 'no_multi_line'], // Supposedly the default, but doesn't behave as such?
-        'new_with_braces'                            => false, // We prefer the opposite to @PhpCsFixer
-        'no_alias_functions'                         => true,
-        'no_blank_lines_after_phpdoc'                => false, // Too many changes for now
-        'no_blank_lines_before_namespace'            => true,
-        'no_break_comment'                           => false, // We prefer the opposite to @PSR2 and @PhpCsFixer
-        'no_extra_blank_lines'                       => false, // Too many changes for now
-        'no_homoglyph_names'                         => true,
-        'no_multiline_whitespace_around_double_arrow'=> false, // Too many changes for now
-        'no_null_property_initialization'            => false, // Too many changes for now
-        'no_php4_constructor'                        => true,
-        'no_short_echo_tag'                          => false, // We prefer the opposite to @PhpCsFixer
-        'no_unneeded_control_parentheses'            => [ // We occasionally use around `return`
+        '@PSR2'                                       => true,
+        '@PhpCsFixer'                                 => true,
+        'array_syntax'                                => ['syntax' => 'short'],
+        'backtick_to_shell_exec'                      => true,
+        'binary_operator_spaces'                      => ['default' => 'align_single_space'],
+        'blank_line_after_opening_tag'                => false, // We prefer the opposite to @PhpCsFixer
+        'blank_line_before_statement'                 => ['statements' => ['case', 'for', 'foreach', 'if', 'return', 'switch', 'try', 'while']],
+        'class_attributes_separation'                 => ['elements' => ['method']], // We like to group const/property
+        'class_keyword_remove'                        => false, // We like IDEs picking up usage via ::class
+        'combine_nested_dirname'                      => true,
+        'concat_space'                                => ['spacing' => 'one'],
+        'date_time_immutable'                         => true,
+        'declare_strict_types'                        => false,
+        'dir_constant'                                => true,
+        'ereg_to_preg'                                => true,
+        'error_suppression'                           => true,
+        'escape_implicit_backslashes'                 => false, // Too many changes for now
+        'function_to_constant'                        => true,
+        'general_phpdoc_annotation_remove'            => ['annotations' => []],
+        'heredoc_indentation'                         => false, // Too many changes for now
+        'heredoc_to_nowdoc'                           => false, // Too many changes for now
+        'implode_call'                                => true,
+        'increment_style'                             => false, // Sometimes either is appropriate
+        'linebreak_after_opening_tag'                 => true,
+        'logical_operators'                           => true,
+        'method_argument_space'                       => ['on_multiline' => 'ignore'],
+        'mb_str_functions'                            => true,
+        'modernize_types_casting'                     => true,
+        'multiline_whitespace_before_semicolons'      => ['strategy' => 'no_multi_line'], // Supposedly the default, but doesn't behave as such?
+        'new_with_braces'                             => false, // We prefer the opposite to @PhpCsFixer
+        'no_alias_functions'                          => true,
+        'no_blank_lines_after_phpdoc'                 => false, // Too many changes for now
+        'no_blank_lines_before_namespace'             => true,
+        'no_break_comment'                            => false, // We prefer the opposite to @PSR2 and @PhpCsFixer
+        'no_extra_blank_lines'                        => false, // Too many changes for now
+        'no_homoglyph_names'                          => true,
+        'no_multiline_whitespace_around_double_arrow' => false, // Too many changes for now
+        'no_null_property_initialization'             => false, // Too many changes for now
+        'no_php4_constructor'                         => true,
+        'no_short_echo_tag'                           => false, // We prefer the opposite to @PhpCsFixer
+        'no_unneeded_control_parentheses'             => [ // We occasionally use around `return`
             'statements' => ['break', 'clone', 'continue', 'echo_print', 'switch_case', 'yield'],
         ],
-        'no_unreachable_default_argument_value'      => true,
-        'non_printable_character'                    => false, // We have these in tests
-        'not_operator_with_successor_space'          => true,
-        'ordered_class_elements'                     => [ // Default, except we don't order methods for now
+        'no_unreachable_default_argument_value'       => true,
+        'non_printable_character'                     => false, // We have these in tests
+        'not_operator_with_successor_space'           => true,
+        'ordered_class_elements'                      => [ // Default, except we don't order methods for now
             'use_trait',
             'constant_public',
             'constant_protected',
@@ -73,48 +71,45 @@ class Factory
             'magic',
             'phpunit',
         ],
-        'ordered_imports'                            => [
-            'importsOrder'  => ['class', 'function', 'const'],
-            'sortAlgorithm' => 'alpha',
-        ],
-        'php_unit_construct'                         => true,
-        'php_unit_dedicate_assert'                   => true,
-        'php_unit_dedicate_assert_internal_type'     => true,
-        'php_unit_expectation'                       => true,
-        'php_unit_internal_class'                    => false, // We prefer the opposite to @PhpCsFixer
-        'php_unit_method_casing'                     => ['case' => 'snake_case'],
-        'php_unit_namespaced'                        => true,
-        'php_unit_no_expectation_annotation'         => true,
-        'php_unit_set_up_tear_down_visibility'       => true,
-        'php_unit_strict'                            => true,
-        'php_unit_test_annotation'                   => true,
-        'php_unit_test_class_requires_covers'        => false, // We prefer the opposite to @PhpCsFixer
-        'phpdoc_add_missing_param_annotation'        => false, // Later, avoiding changes for now
-        'phpdoc_align'                               => false, // Later, avoiding changes for now
-        'phpdoc_annotation_without_dot'              => false, // Later, avoiding changes for now
-        'phpdoc_indent'                              => false, // Later, avoiding changes for now
-        'phpdoc_inline_tag'                          => false, // Later, avoiding changes for now
-        'phpdoc_no_alias_tag'                        => false, // Later, avoiding changes for now
-        'phpdoc_order'                               => false, // Later, avoiding changes for now
-        'phpdoc_return_self_reference'               => false, // Later, avoiding changes for now
-        'phpdoc_separation'                          => false, // Later, avoiding changes for now
-        'phpdoc_summary'                             => false, // Later, avoiding changes for now
-        'phpdoc_to_comment'                          => false, // Later, avoiding changes for now
-        'phpdoc_trim'                                => false, // Later, avoiding changes for now
-        'protected_to_private'                       => false,
-        'psr4'                                       => true,
-        'random_api_migration'                       => true,
-        'self_accessor'                              => true,
-        'set_type_to_cast'                           => true,
-        'simplified_null_return'                     => true,
-        'single_blank_line_before_namespace'         => false, // We prefer no_blank_lines_before_namespace
-        'single_import_per_statement'                => false, // We like import grouping within the same namespace
-        'single_quote'                               => false, // Later, avoiding changes for now
-        'single_trait_insert_per_statement'          => false, // We like grouping traits in one statement
-        'strict_comparison'                          => true,
-        'ternary_to_null_coalescing'                 => true,
-        'void_return'                                => true,
-        'yoda_style'                                 => false, // We prefer the opposite to @PhpCsFixer
+        'ordered_imports'                             => ['importsOrder' => ['class', 'function', 'const'], 'sortAlgorithm' => 'alpha'],
+        'php_unit_construct'                          => true,
+        'php_unit_dedicate_assert'                    => true,
+        'php_unit_dedicate_assert_internal_type'      => true,
+        'php_unit_expectation'                        => true,
+        'php_unit_internal_class'                     => false, // We prefer the opposite to @PhpCsFixer
+        'php_unit_method_casing'                      => ['case' => 'snake_case'],
+        'php_unit_namespaced'                         => true,
+        'php_unit_no_expectation_annotation'          => true,
+        'php_unit_set_up_tear_down_visibility'        => true,
+        'php_unit_strict'                             => true,
+        'php_unit_test_annotation'                    => true,
+        'php_unit_test_class_requires_covers'         => false, // We prefer the opposite to @PhpCsFixer
+        'phpdoc_add_missing_param_annotation'         => false, // Later, avoiding changes for now
+        'phpdoc_align'                                => false, // Later, avoiding changes for now
+        'phpdoc_annotation_without_dot'               => false, // Later, avoiding changes for now
+        'phpdoc_indent'                               => false, // Later, avoiding changes for now
+        'phpdoc_inline_tag'                           => false, // Later, avoiding changes for now
+        'phpdoc_no_alias_tag'                         => false, // Later, avoiding changes for now
+        'phpdoc_order'                                => false, // Later, avoiding changes for now
+        'phpdoc_return_self_reference'                => false, // Later, avoiding changes for now
+        'phpdoc_separation'                           => false, // Later, avoiding changes for now
+        'phpdoc_summary'                              => false, // Later, avoiding changes for now
+        'phpdoc_to_comment'                           => false, // Later, avoiding changes for now
+        'phpdoc_trim'                                 => false, // Later, avoiding changes for now
+        'protected_to_private'                        => false,
+        'psr4'                                        => true,
+        'random_api_migration'                        => true,
+        'self_accessor'                               => true,
+        'set_type_to_cast'                            => true,
+        'simplified_null_return'                      => true,
+        'single_blank_line_before_namespace'          => false, // We prefer no_blank_lines_before_namespace
+        'single_import_per_statement'                 => false, // We like import grouping within the same namespace
+        'single_quote'                                => false, // Later, avoiding changes for now
+        'single_trait_insert_per_statement'           => false, // We like grouping traits in one statement
+        'strict_comparison'                           => true,
+        'ternary_to_null_coalescing'                  => true,
+        'void_return'                                 => true,
+        'yoda_style'                                  => false, // We prefer the opposite to @PhpCsFixer
     ];
 
     public static function fromDefaults(array $overrideRules = []): Config
